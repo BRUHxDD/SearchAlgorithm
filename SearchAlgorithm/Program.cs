c1e634df166029682f4ee8090d0d24da1e4b7a97
Teuku Muhammad Attar Fadhilah <teuku.muhammad.ft21@mail.umy.ac.id>
2022-10-24 16:15:22 +0700

    Menambahkan int array

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -3,3 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+namespace SearchAlgorithm
+{
+    class Program
+    {
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements in the array 
+        int n;
+        //Get the number of elements to store in the array
+        int i;
+    }
+}
